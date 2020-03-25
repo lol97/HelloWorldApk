a2a4ed617532269a3c95518929b49a8c0fe194cc
sufyan <blackmanga80@gmail.com>
2020-03-25 17:26:44 +0700

    init push

---
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -0,0 +1,39 @@
+// based on https://www.codepolitan.com/belajar-membuat-aplikasi-android-dengan-kotlin-untuk-pemula-bagian-1-activity-dan-layout-5aaa3f51344be
+package com.example.helloworld
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu
+import android.view.MenuItem
+
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+
+        btnMasuk.setOnClickListener { view ->
+            inputRes.text = inputData.text
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+}
